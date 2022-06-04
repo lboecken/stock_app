624a7a33395f800cd804dc0c92f766ae0cef070b
Crystal Word <cword1991@gmail.com>
2022-06-04 01:10:15 -0400

    adding base server functionality

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+// import SignInForm from './Components/SignInForm';
+// import RegisterForm from './Components/RegisterForm';
+// import Homepage from './Components/Homepage';
+// import Dashboard from './Components/Dashboard'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import TradePage from './Components/TradePage';
+// import PortfolioPage from './Components/PortfolioPage';
+import { Outlet } from "react-router-dom";
+
+
+function App() {
+
+  // const useToken = () => {
+  //   console.log("useToken")
+  // }
+  // const useUser = () => {
+  //   console.log("useUser")
+  // }
+
+  // const {token, setToken, removeToken} = useToken();
+  // const {signedInUser, setSignedInUser, signOutUser } = useUser(); 
+
+  // const CONTEXT = {
+  // token: token, 
+  // setToken, 
+  // signedInUser,
+  // removeToken,
+  // signOutUser
+  // };
+
+
+  return (
+    <>
+      {/* <Outlet context={CONTEXT}/> */}
+      <Outlet/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {BrowserRouter, Routes, Route, useOutlet, useOutletContext, Navigate, Outlet } from 'react-router-dom';
+import SignInForm from './Components/SignInForm';
+import RegisterForm from './Components/RegisterForm';
+import Homepage from './Components/Homepage';
+import Dashboard from './Components/Dashboard'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import TradePage from './Components/TradePage';
+import PortfolioPage from './Components/PortfolioPage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   <BrowserRouter>
+      <>
+    <Routes>
+    <Route element={<App />}>
+
+    {/* <Route path="/" element={<PublicRoute restricted element={<Homepage />}/>} /> */}
+    {/* <Route path="/" element={<PublicRoute restricted element={<SignInForm />}/>} /> */}
+    {/* <Route path="/" element={<PublicRoute restricted element={<RegisterForm />}/>} /> */}
+    <Route path="/" element={<Homepage />} />
+    <Route path="/signin" element={<SignInForm />} />
+    <Route path="/register" element={<RegisterForm />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/trade" element={<TradePage />} />
+    <Route path="/portfolio" element={<PortfolioPage />} />
+     {/* <Route path="/" element={<PrivateRoute element={<Dashboard />}/>} /> */}
+    {/* <Route path="/" element={<PrivateRoute element={<TradePage/>}/>} /> */}
+    {/* <Route path="/" element={<PrivateRoute element={<PortfolioPage />}/>} /> */}
+
+    </Route>
+    </Routes>
+
+</>
+      </BrowserRouter>
+
+    {/* <App /> */}
+  </React.StrictMode>
+);
+
+// const PublicRoute = ( { restricted, element }) => {
+//   const { token } = useOutletContext();
+//   if (token && restricted) {
+//     return <Navigate to="./dashboard" replace={true} />;
+//   } else {
+//     return element ? element : <Outlet />
+//   }
+//   }
+// const PrivateRoute = ({ element }) => {
+//   const { token } = useOutletContext();
+//   if (token) {
+//     return element ? element : <Outlet />;
+//   } else {
+    
+//   } return <Navigate to="../" replace={true} />;
+// }
