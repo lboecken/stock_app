810172568531b9ea4d6514d631073b0029a714d7
Crystal Word <cword1991@gmail.com>
2022-07-03 12:26:44 -0400

    adding create_cash_balance_record to crud module

--- server/cash_balance_model.py
+++ server/cash_balance_model.py
@@ -4,6 +4,7 @@ class Cash_Balance(db.Model):
     __tablename__ = "cash_balance"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    username = db.Column(db.String, db.ForeignKey('users.username'))
     # the_user = db.relationship("User", backref=backref("users", uselist=False))
     cash_balance = db.Column(db.Float, default=100000)
     
--- server/crud.py
+++ server/crud.py
@@ -4,6 +4,7 @@ from server.db_connection import db
 # from server.crud import *
 from server.user_model import User
 from server.holdings_model import Holdings
+from server.cash_balance_model import Cash_Balance
 
 def create_user_connection(username, password):
 
@@ -12,6 +13,8 @@ def create_user_connection(username, password):
     new_user = User(username=username, password=stringed_hashed_password)
     db.session.add(new_user)
     db.session.commit()
+    new_user_id = new_user.id
+    create_cash_balance_record(new_user_id, username)
 
     return 'User Created'
 
@@ -22,3 +25,11 @@ def create_holdings_record(params):
     db.session.commit()
 
     return 'Holdings Record Created'
+
+def create_cash_balance_record(userid, username):
+
+    cash_balance_record = Cash_Balance(user_id=userid, username=username)
+    db.session.add(cash_balance_record)
+    db.session.commit()
+
+    return 'Cash Balance Intialized'
