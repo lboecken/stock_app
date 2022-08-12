061f6fa26e53a32918ea78f100e405fb2556d65b
Lennart Boecken <lsboecken@gmail.com>
2022-08-12 09:57:10 -0400

    init refactor

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# from server import create_app
+
 from flask import send_from_directory
 from server import create_app
-# from . import create_app
+
 from flask_sqlalchemy import SQLAlchemy
-# from flask_cli import FlaskGroup
+
 from dotenv import load_dotenv
 load_dotenv()
 import os
-# from flask_cors import CORS
+
 
 app = create_app()
-# CORS(app)
+
 
 
 @app.route("/", defaults={"path": ""})
@@ -24,8 +24,4 @@ if __name__ == "__main__":
 
 
 
-# cli = FlaskGroup(create_app=create_app)
-# @app.cli.command('resetdb')
-# def recreate_db():
-#     db.drop_all()
-#     db.create_all()
+
